d5279f51355b6e8c97b2518bcc2d8c75dc0e58ef
sssam04 <sssam@ug.edu.gh>
2022-09-12 16:02:18 -0700

    0-positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,11 +8,8 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 {
 	printf("%d is positive\n", n);
@@ -27,4 +24,3 @@ printf("%d is zero\n", n);
 }
 	return (0);
 }
-
