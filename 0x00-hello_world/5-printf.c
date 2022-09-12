e6ef2ad07292b3f87c6b05c4e05c60dd9f1cf504
sssam04 <sssam@ug.edu.gh>
2022-09-12 13:07:43 -0700

    5-print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+  *
+  * main - main block
+  * Return: 0
+  */
+int main(void)
+{
+	Print("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
+}
