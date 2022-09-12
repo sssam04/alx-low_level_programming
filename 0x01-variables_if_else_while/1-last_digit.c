4f2702e10b1c2c5f3426cc2eb7fe6e07a53a116a
sssam04 <sssam@ug.edu.gh>
2022-09-12 15:19:20 -0700

    0-positive or negative

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,26 +1,25 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
-  *  * main - Determine if the last digit of a random number is
-  *   * greater than or less than 5, or is zero..
-  *   (*
-  *    * Return: 0 on success
+  *  * main - Entry point
+  *   *
+  *    * Description: print all aplhabet letters
+  *     *
+  *      * Return: Always 0 (success)
   */
-  int main(void)
-  {
-	  int n;
-	  char last[] = "Last digit of";
 
-	  srand(time(0));
+int main(void)
+{
 
-	  n = rand() - RAND_MAX / 2;
-	  printf("%s %d is %d and is ", last, n, n % 10);
-	  if (n % 10 > 5)
-		  printf("greater than 5\n");
-	  else if (n % 10 == 0)
-		  printf("0\n");
-	  else
-		  printf("less than 6 and not 0\n");
-	  return (0);
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	
+	{
+	putchar (alpha);
+	alpha ++;
+	}
+	putchar('\n');
+	return (0);
+
 }
