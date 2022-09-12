11b044191b71f4a62ba6434fab3f1065d7644779
sssam04 <sssam@ug.edu.gh>
2022-09-12 12:38:50 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
- #include <stio.h>
- /**
-    * main - Entry point
+#include <stio.h>
+/**
+   * main - Entry point
      *
        * Return: always 0
          */
- int main (void)
- {
-		puts ("\"Programming is like building a multilingual puzzle");
-			return (0);
- }
+int main (void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
