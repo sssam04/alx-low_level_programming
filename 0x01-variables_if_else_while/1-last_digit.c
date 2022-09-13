a92136a90d3d280065da2ae36b7a45be52004854
sssam04 <sssam@ug.edu.gh>
2022-09-12 17:15:58 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,16 +9,15 @@
   */
 int main(void)
 {
-
-	char alpha = 'a';
+char alpha = 'a';
 
 	while (alpha <= 'z')
 	
 {
-putchar (alpha);
-alpha ++;
+	putchar (alpha);
+	alpha ++;
 }
-putchar('\n');
+	putchar('\n');
 	return (0);
 
 }
