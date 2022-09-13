cbbb2c68371fb7456df94a110ff965918222bf09
sssam04 <sssam@ug.edu.gh>
2022-09-12 17:06:18 -0700

    0-positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,14 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <time.h>
- /**
+
+/**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
+int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
