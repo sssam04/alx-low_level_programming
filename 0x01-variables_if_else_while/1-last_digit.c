7716eaeb3e840ea80c5ce070cffc2649144e5342
sssam04 <sssam@ug.edu.gh>
2022-09-15 01:55:33 -0700

    1-last gigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
-  *  * main - Entry point
-  *   *
-  *    * Description: print all aplhabet letters
-  *     *
-  *      * Return: Always 0 (success)
+  *  main - Entry point
+  *   
+  *  Description:Finding if a number is greater or less than 5 or is zero
+  * 
+  *  Return: Always 0 (success)
   */
 int main(void)
 {
-char alpha = 'a';
+	int n;
+	char last[] = "Last digit of";
 
-	while (alpha <= 'z')
-	
-{
-	putchar (alpha);
-	alpha ++;
-}
-	putchar('\n');
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+	printf("%s %d is %d and is ", last, n, n % 10);
+	if (n % 10 > 5)
+	printf("greater than 5\n");
+	else if (n % 10 == 0)
+	printf("0\n");
+
+	else
+		printf("less than 6 and not 0\n");
 	return (0);
-
 }
