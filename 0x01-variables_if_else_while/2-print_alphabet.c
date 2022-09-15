537c3d1c70cf2d929b1ae0c210e37ab0ef609987
sssam04 <sssam@ug.edu.gh>
2022-09-15 02:46:56 -0700

    -print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,26 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
- *  * main - Determine if the last digit of a random number is
- *   * greater than or less than 5, or is zero..
- *   (* 
+ *  * main - Entry point
+ *   * 
+ *  (* 
  *    *  Return: 0 on success 
  */
 
  int main(void)
  {
-	       char char last[] = "Last digit of";
+	      char alpha = 'a';
+	      while (alpha <= 'z')
 
-	       srand(time(0));
-
-	       n = rand() - RAND_MAX / 2;
-	       printf("%s %d is %d and is ", last, n, n % 10);
-	       printf("%s %d is %d and is ", last, n, n % 10);
-	       printf("greater than 5\n");
-	       else if (n % 10 == 0)
-		       printf("0\n");
-	       else
-		       printf("less than 6 and not 0\n");
-	       return (0)
+	      {
+		      putchar (alpha);
+		      alpha++;
+		}
+	      putchar('\n');
+	      return (0)
 }
