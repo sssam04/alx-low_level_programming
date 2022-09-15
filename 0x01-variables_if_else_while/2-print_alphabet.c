3f259d359c0fc9bd32246a641d3da4fb6b55b875
sssam04 <sssam@ug.edu.gh>
2022-09-15 03:48:58 -0700

    2-print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,16 +8,17 @@
  *   * Return: Always 0 (Success)
  */
 
-
 int main(void)
 {
+
 	char alpha = 'a';
+	
 	while(alpha <= 'z')
-{
+
+	{
 	putchar(alpha);
 	alpha++;
 }
 	putchar('\n');
 	return (0);
-
 }
