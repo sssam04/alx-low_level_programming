a5838c4b77e0a7515dbc0d451db9ddccbe562b1c
sssam04 <sssam@ug.edu.gh>
2022-09-15 22:59:37 -0700

    2-mul-c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,7 +3,8 @@
  
 
 /** 
- * mul - multiplies two integers 
+ * mul - multiplies
+ * two integers 
  * 
  * @a: first input 
  * @b: second input 
@@ -12,7 +13,7 @@
  */ 
  
 
-	int mul(int a, int b) 
+int mul(int a, int b) 
 
 { 
 
