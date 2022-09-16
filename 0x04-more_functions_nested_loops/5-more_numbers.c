64261118b820948d843fb34e50e63f39250056bc
sssam04 <sssam@ug.edu.gh>
2022-09-16 02:52:42 -0700

    5-more-numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,17 +2,17 @@
 
 
 /**
-
- * more-numbers - prints 10 times the numbers, from 0 to 14
+ * more-numbers -  you are to print
+ * 10 times the numbers, from 0 to 14
  * followed by a new line
  */
 
 void more_numbers(void)
-	
+
 {
 
 	int i, j;
-	 
+
 	for (i = 0; i < 10; i++)
 
 	{
