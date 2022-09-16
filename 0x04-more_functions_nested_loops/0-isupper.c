026e6a8777590654cd7f016b8e8a92717c3c5a92
sssam04 <sssam@ug.edu.gh>
2022-09-15 22:00:18 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,23 +5,14 @@
  
 
 /** 
-
-  * _isupper - function to check if 
-
-  * character is uppercase 
-
-  * 
-
-  * @c: checks input of function 
-
-  * 
-
-  * Return: returns 1 if `c` is uppercase 
-
-  * otherwise always 0 (Success) 
-
-  */ 
-
+ * _isupper - function to check if 
+ * character is uppercase 
+ * 
+ * @c: checks input of function 
+ * 
+ * Return: returns 1 if `c` is uppercase 
+ * otherwise always 0 (Success) 
+ */ 
  
 
 int _isupper(int c) 
