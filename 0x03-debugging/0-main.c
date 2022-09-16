3eb3f0588611b8637c88c8c622177df3064b6771
sssam04 <sssam@ug.edu.gh>
2022-09-16 08:56:35 -0700

    0-main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,17 @@
 #include "main.h"
 
 /**
-
  * main - prints positive or negative integer
-
  * Return: 0
-
  */
 
 int main(void)
-
 {
 
-		int i;
+	int i;
 
-			i = 0;
+	i = 0;
 
-				positive_or_negative(i);
-
-					return (0);
-
+	positive_or_negative(i);
+		return (0);
 }
-
-
