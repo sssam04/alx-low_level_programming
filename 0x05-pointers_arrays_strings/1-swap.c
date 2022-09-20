02074cefa6698665c7f2dbd60c2875b3331acd3f
sssam04 <sssam@ug.edu.gh>
2022-09-19 23:01:56 -0700

    1-swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,17 +3,23 @@
 
 
 /**
- *reset_to_98 - resets the value of given integer to 98
- *@n: parameter
- *Return : nothing
+ *swap_int - swaps two integers' values
+ *@a: first integer
+ *@b: second integer
+ *Return: returns nothing
  */
 
 
 
-void reset_to_98(int *n)
+void swap_int(int *a, int *b)
 
 {
 
-		*n = 98;
+		int tmp = *a;
 
+
+
+		*a = *b;
+
+		*b = tmp;
 }
